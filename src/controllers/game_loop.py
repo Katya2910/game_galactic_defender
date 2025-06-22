26299708c46d4931b66165cbf66294713dc51a0b
KatyaH2910 <ekd2910@mail.ru>
2025-06-22 13:27:48 +0500

    my_gamee

--- src/controllers/game_loop.py
+++ src/controllers/game_loop.py
@@ -5,7 +5,7 @@ from src.config.config import Config
 def cycle_cosmos(game):
     while game.running:
         game.event_handler.process_signals()
-        # Явная проверка состояния
+        # Проверка состояния
         if game.game_state == 1 or game.game_state == 2 or game.game_state == 3:
             game.object_manager.advance_simulation()
         game.render_manager.project_scene()
